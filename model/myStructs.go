5e112da5959ecaa65e726522e510551656ebbe74
la02w <1786075785@qq.com>
2024-10-25 16:47:48 +0800

    修改结构体接口响应的状态，添加注释

--- model/myStructs.go
+++ model/myStructs.go
@@ -1,33 +1,23 @@
 package model
 
+// 服务器在线列表响应体
 type ClientList struct {
 	Count int `json:"count"`
 	Body  []struct {
 		ClientNickname string `json:"client_nickname"`
 	} `json:"body"`
-	Status struct {
-		Code int    ` json:"code"`
-		Msg  string ` json:"message"`
-	} `json:"status"`
+	Status StatusInfo `json:"status"`
 }
+
+// 创建频道响应体
 type ChannelData struct {
 	Body []struct {
 		CID string `json:"cid"`
 	} `json:"body"`
-	Status struct {
-		Code int    ` json:"code"`
-		Msg  string ` json:"message"`
-	} `json:"status"`
-}
-
-type TsJsonData struct {
-	Body   []Body     `json:"body"`
 	Status StatusInfo `json:"status"`
 }
-type Body struct {
-	ClientNickname string `json:"client_nickname,omitempty"`
-	CID            string `json:"cid"`
-}
+
+// 状态响应体
 type Status struct {
 	Status StatusInfo ` json:"status"`
 }
@@ -35,6 +25,8 @@ type StatusInfo struct {
 	Code int    ` json:"code"`
 	Msg  string ` json:"message"`
 }
+
+// 创建频道请求体
 type ChannelInfo struct {
 	ServerId        string `json:"sid"`
 	ChannelName     string `json:"channel_name"`
